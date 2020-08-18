3e14e39146d0a7d57044b00cc800225dcad5e950
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-08-18 18:07:06 -0400

    mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,26 @@
 def linear_search(arr, target):
-    # Your code here
-
-
+    for x in range(len(arr)):
+        if arr[x] == target:
+            return x
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1 
+    mid = 0
 
-    # Your code here
+    while low <= high:
+        mid = (high + low) // 2
+        if arr[mid] < target:
+            low = mid + 1
+        
+        elif arr[mid] > target:
+            high = mid -1 
+        
+        else:
+            return mid 
 
 
     return -1  # not found
